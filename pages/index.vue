53a340ca254ccd26d5f435a722fdb3d537290e32
Anton Rybkin <n1n1@bk.ru>
2018-12-28 20:10:00 -0500

    static page + post request bug

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,13 @@
     <div class="calc__last-results">
       <div class="calc__last-results__wrapper">
         <h2>Last results:</h2>
-        <p
-          v-for="res in lastResults.slice(lastResults.length - 6, lastResults.length).reverse()"
-          :key="res.id">
-          <b>{{ res.calculation }}</b>
-        </p>
+        <no-ssr>
+          <p
+            v-for="res in lastResults.slice(lastResults.length - 6, lastResults.length).reverse()"
+            :key="res.id">
+            <b>{{ res.calculation }}</b>
+          </p>
+        </no-ssr>
       </div>
     </div>
   </div>
